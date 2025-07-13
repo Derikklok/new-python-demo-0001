78674c8c54c6fe5d290ba8153ac6aa0c89730492
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-07-13 18:07:54 +0530

    update greeting in main.py to personalize output

---
+++ app.py
@@ -0,0 +1,14 @@
+print("I like pizza!")
+
+# comment
+# python
+# does not have a way to
+# represent multiline comments
+
+if __name__ == '__main__':
+    print('hello from app.py')
+
+# Variables
+num1 = 10
+num2 = float(num1)
+print(num2)
